8e05318896bbcca50b5a59a4e7483f7ab736c232
med-men <med-men@gmx.net>
2017-06-15 23:37:00 +0200

    new input field now seems to work well with date selection

--- js/jq_new-inputfield.js
+++ js/jq_new-inputfield.js
@@ -1,23 +1,39 @@
-$(document).on('change', 'input', function() {
-    console.log('triggered checkin if new field should be created');
-    var emptyfields = 0;
-    var filledfields = 0;
-    var placeholder = '';
+$(document).on('blur change', 'input', function() {
+    // console.log('triggered checkin if new field should be created');
+    var this_field = $(this);
+    var placeholder = this_field.attr('placeholder');
+    var cnt = 1;
 
-    // delete unnecessary empty fields
-    $(this).parent().children(':text').each( function() {
+    // delete unnecessary empty fields, except the we are working on 
+    this_field.siblings(':text').each( function() {
        if($(this).val().length < 1) {
-           console.log('deleting an empty field');
-           $(this).remove();
-       } else {
-           placeholder = $(this).attr('placeholder');
+           console.log('deleting empty field with id ' + $(this).attr('id') +' and its script');
+           var script_id = 's' + $(this).attr('id');
+           $("#" + script_id).remove(); // delete attached script
+           $(this).remove(); // delete input field
        }
     });
 
+    function find_unused_id(f_parent, cnt) {
+        var check_id = f_parent.attr('id') + '_' + cnt;
+        console.log('checking if id ' + check_id + ' is already taken');
+        if($("#" + check_id).length == 0) {
+            // no field with id = check_id found, we can use this one
+            return check_id;
+        } else {
+            return false;
+        }
+    };
 
-    console.log('NOW i will add a SINGLE empty field');
-    // var allfields = $(this).parent().children(':text').length;
-    var field_id = 110;
-    $(this).parent().append('<input placeholder="' + placeholder +'" class="datepicker" type="text" size="15" name="' + $(this).parent().attr('id') + '[]" id="' + $(this).parent().attr('id') + '_' + field_id +'"/> <script> $(function() { $("#'+ $(this).parent().attr('id') + '_' + field_id + '").dateRangePicker(); });</script>'); //add input box
+    var new_id = find_unused_id(this_field.parent(), cnt);
+    // add single empty field, find unique id for that
+    while( new_id === false) {
+        cnt++;
+        new_id = find_unused_id(this_field.parent(), cnt);
+    }
+
+
+    console.log('adding new field with id ' + new_id + ' to ' + this_field.attr('id'));
+    this_field.parent().append('<input placeholder="' + placeholder +'" class="datepicker" type="text" size="15" name="' + $(this).parent().attr('id') + '[]" id="' + new_id + '"/> <script id="s' + new_id + '"> $(function() { $("#' + new_id + '").dateRangePicker(); });</script>'); //add input box
 });
 
