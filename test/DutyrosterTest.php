15398afdb15ee603d9083935f5f4c932e7c92c2b
med-men <med-men@gmx.net>
2023-08-06 20:38:15 +0200

    first working Test

--- test/DutyrosterTest.php
+++ test/DutyrosterTest.php
@@ -4,78 +4,72 @@ declare(strict_types=1);
 namespace Dienstplan\Test;
 
 use PHPUnit\Framework\TestCase;
-use Prophecy\PhpUnit\ProphecyTrait;
+# use Prophecy\PhpUnit\ProphecyTrait;
 use Dienstplan\Worker\Dutyroster;
 
+/**
+ * Setup: extend Dutyroster class to add a setter for config data
+ */
+class DutyrosterMock extends Dutyroster {
+    public function set_config_data(array $data):void {
+        $this->config = $data;
+    }
+}
+
 class DutyrosterTest extends TestCase {
-    use ProphecyTrait;
+  #  use ProphecyTrait;
 
-    private $config;
-
-    protected function setUp():void
+    public function getConfig()
     {
-        $this->config['people'] = [
-            'anton'  => ['fullname' => 'Anton Anders', 'pw' => '$2y$10$cv0fitJNDmQdzydZBGcW7eBYqmwqcpSQWMOqt/FiFrTthVqHZqHD6'], // pw chaf666
-            'berta'  => ['fullname' => 'Berta Besonders', 'pw' => '$2y$10$cv0fitJNDmQdzydZBGcW7eBYqmwqcpSQWMOqt/FiFrTthVqHZqHD6', 'is_admin' => true],
-            'conny'  => [],
-            'dick'   => [],
-            'egon'   => [],
-            'floppy' => [],
-            'guste'  => [],
-        ];
-
-        $this->config['wishes'] = [
-            'anton' => [
-                "1"  => "D",
-                "4"  => "F",
-                "8"  => "F",
-                "14" => "F",
-            ],
-            'berta' => [
-                "3"  => "F",
-                "4"  => "F",
-                "8"  => "D",
-                "12" => "F",
-            ]
-        ];
-
-        $this->config['urlaub'] = [
-            'anton' => [
-                '05.02.2017 ~ 15.02.2017',
-                '21.03.2017',
-                '15.05.2017 ~ 25.05.2017'
-            ],
-            'berta' => [
-                '10.02.2017 ~ 20.02.2017'
-            ]
-        ];
-
-        $this->config['limits'] = [
-            'total'          => 5,
-            'we'             => 2,
-            'fr'             => 1,
-            'max_iterations' => 500
-        ];
+        return [
+                'people' => [
+                    'anton'  => ['fullname' => 'Anton Anders', 'pw' => '$2y$10$cv0fitJNDmQdzydZBGcW7eBYqmwqcpSQWMOqt/FiFrTthVqHZqHD6'], // pw chaf666
+                    'berta'  => ['fullname' => 'Berta Besonders', 'pw' => '$2y$10$cv0fitJNDmQdzydZBGcW7eBYqmwqcpSQWMOqt/FiFrTthVqHZqHD6', 'is_admin' => true],
+                    'conny'  => [],
+                    'dick'   => [],
+                    'egon'   => [],
+                    'floppy' => [],
+                    'guste'  => [],
+                ],
+                'wishes' => [
+                    'anton' => [
+                        "1"  => "D",
+                        "4"  => "F",
+                        "8"  => "F",
+                        "14" => "F",
+                    ],
+                    'berta' => [
+                        "3"  => "F",
+                        "4"  => "F",
+                        "8"  => "D",
+                        "12" => "F",
+                    ]
+                ],
+                'urlaub' => [
+                    'anton' => [
+                        '05.02.2017 ~ 15.02.2017',
+                        '21.03.2017',
+                        '15.05.2017 ~ 25.05.2017'
+                    ],
+                    'berta' => [
+                        '10.02.2017 ~ 20.02.2017'
+                    ]
+                ],
+                'limits' => [
+                    'total'          => 5,
+                    'we'             => 2,
+                    'fr'             => 1,
+                    'max_iterations' => 500
+                ]
+       ];
     }
 
-    public function test_has_noduty_wish(){
-        $dutyroster = $this->prophesize(Dutyroster::class);
+    public function test_has_noduty_wish():void {
+        $fixed_datetime = \DateTime::createFromFormat("m/Y", "03/2023");
 
-        // override $config
-        $this->assertTrue($dutyroster->has_noduty_wish('anton', 4));
-    }
+        $dutyroster_mock = new DutyrosterMock($fixed_datetime);
+        $dutyroster_mock->set_config_data($this->getConfig());
 
-    public function test_find_candidate() {
-        /**
-         * Complex SetUp:
-         * Given I have an array of name => date
-         */
-        $now = new \DateTimeImmutable();
-        $day =13;
-
-        $dutyroster = $this->prophesize(Dutyroster::class);
-
-        // override $config
-         $this->assertEquals('anton', $dutyroster->find_candidate($day));
+        $this->assertTrue($dutyroster_mock->has_noduty_wish('anton', 4));
     }
 }
