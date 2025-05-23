940b0dd0a9796228cfd617941ace7d09a0b267ca
medmen <med-men@gmx.net>
2025-05-23 14:19:22 +0200

    Update people.php

--- data/people.php
+++ data/people.php
@@ -2,7 +2,7 @@
 <?php
 
 return( array(
-        'anton' => ['fullname' => 'Anton Anders', 'pw' => '$2y$10$cv0fitJNDmQdzydZBGcW7eBYqmwqcpSQWMOqt/FiFrTthVqHZqHD6'], // pw chaf666
+        'anton' => ['fullname' => 'Anton Anders', 'pw' => '$2y$10$cv0fitJNDmQdzydZBGcW7eBYqmwqcpSQWMOqt/FiFrTthVqHZqHD6'], // generate you own pw here
         'berta' => ['fullname' => 'Berta Besonders', 'pw' => '$2y$10$cv0fitJNDmQdzydZBGcW7eBYqmwqcpSQWMOqt/FiFrTthVqHZqHD6', 'is_admin' => true],
         'conny' => ['fullname' => 'Cornelia Chaos'],
         'dick' => ['firstname' => 'Dirk', 'lastname' => 'Ickinger'],
